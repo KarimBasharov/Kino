f38c7dba09e8142724ab97c3d41337e91170c730
Opilane TTHK <opilane@tthk.ee>
2021-02-03 15:44:40 +0200

    Add project files.

---
+++ Kinozal/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kinozal
+{
+    public partial class Form1 : Form
+    {
+        Button btn1, btn2, btn3;
+        Label lbl;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Height = 191;
+            this.Width = 367;
+            btn1 = new Button
+            {
+                Text = "Маленький",
+                Location = new Point(11, 70),
+                Height = 40,
+                Width = 100
+            };
+            this.Controls.Add(btn1);
+            btn1.Click += Btn1_Click;
+            btn2 = new Button
+            {
+                Text = "Средний",
+                Location = new Point(111, 70),
+                Height = 40,
+                Width = 100
+            };
+            this.Controls.Add(btn2);
+            btn2.Click += Btn2_Click;
+            btn3 = new Button
+            {
+                Text = "Большой",
+                Location = new Point(211, 70),
+                Height = 40,
+                Width = 100
+            };
+            this.Controls.Add(btn3);
+            btn3.Click += Btn3_Click;
+            lbl = new Label
+            {
+                Text = "Выберите размер кинозала",
+                Location = new Point(22, 30),
+                Width = 280,
+            };
+            lbl.Font = new System.Drawing.Font("Arial", 10, System.Drawing.FontStyle.Bold);
+            this.Controls.Add(lbl);
+        }
+
+        private void Btn3_Click(object sender, EventArgs e)
+        {
+            Bigg bigg = new Bigg();
+            bigg.Show();
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            Mediumm mediumm = new Mediumm();
+            mediumm.Show();
+        }
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            Smalll small = new Smalll();
+            small.Show();
+        }
+
+    }
+}
---
+++ Kinozal/Smalll.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kinozal
+{
+    public partial class Smalll : Form
+    {
+        PictureBox r1, r2, r3, r4, r5, r6;
+        public Smalll()
+        {
+            InitializeComponent();
+            this.Height = 489;
+            this.Width = 816;
+
+            r1 = new PictureBox
+            {
+                Height = 52,
+                Width = 52,
+                BackColor = Color.Green
+            };
+            this.Controls.Add(r1);
+            r1.Click += R1_Click;
+            r2 = new PictureBox
+            {
+                Location = new Point(55, 0),
+                Height = 52,
+                Width = 52,
+                BackColor = Color.Green
+            };
+            this.Controls.Add(r2);
+            r3 = new PictureBox
+            {
+                Location = new Point(110, 0),
+                Height = 52,
+                Width = 52,
+                BackColor = Color.Green
+            };
+            this.Controls.Add(r3);
+        }
+
+        private void R1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Wow");
+        }
+    }
+}
